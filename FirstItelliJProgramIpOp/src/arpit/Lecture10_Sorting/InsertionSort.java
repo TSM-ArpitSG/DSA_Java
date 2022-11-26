8e74ad37c1e8d369cbae17655a2393a273fee389
TSM-ArpitSG <arpit.singh2019@vitbhopal.ac.in>
2022-11-26 13:28:39 +0530

    Insertion Sort Added

---
+++ FirstItelliJProgramIpOp/src/arpit/Lecture10_Sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package arpit.Lecture10_Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,4,6};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }else break;
+            }
+        }
+        return;
+    }
+
+}
---
+++ FirstItelliJProgramIpOp/src/arpit/Lecture10_Sorting/SelectionSort.java
@@ -0,0 +1,36 @@
+package arpit.Lecture10_Sorting;
+
+import java.util.Arrays;
+
+
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selectionSort(int[] arr) {
+        int n =arr.length;
+        for(int i=0;i<n-1;i++){
+          int lastidx = n-i-1;
+          int mxidx = getMaxIndex(arr,lastidx);
+          int temp = arr[lastidx];
+          arr[lastidx]=arr[mxidx];
+          arr[mxidx]=temp;
+        }
+    }
+
+    private static int getMaxIndex(int[] arr,int last) {
+        int idx=-1;
+        int mx=Integer.MIN_VALUE;
+        for(int i=0;i<=last;i++){
+            if(arr[i]>mx){
+                mx = arr[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
